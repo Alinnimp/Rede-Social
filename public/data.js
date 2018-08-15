746d71ff08534bb1aadbe8bf751e7efe11cbeb2d
Carol Gonzalez <carolzinha.mgonzalez@gmail.com>
2018-08-15 19:08:35 -0300

    versao 0.2.1

--- public/data.js
+++ public/data.js
@@ -1,78 +1,46 @@
  var database = firebase.database();
 
 $(document).ready(function(){
+  database.ref('posts').once('value').then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot){
+      var childKey = childSnapshot.key;
+      var childData = childSnapshot.val();
+      var typePost = $('.typePost').val();
+      posts(childData.text, typePost, childKey);
+    });
+  });
 
-$('#btn-post').click(function(event){
-  event.preventDefault();
-
-  var value = $('#post').val();
-  var typePost = $('.typePost').val();
-  $('.listPosts').append(`<article>${value}<p>${typePost}</p></article>`);
-  database.ref('posts/' + userId).push({
-     text: value,
-     select: typePost,
-  })
-  $('#post').val('');
-});
-
+  $('#btn-post').click(function(event){
+    event.preventDefault();
+    var value = $('#post').val();
+    var typePost = $('.typePost').val();
+    var dataNew = database.ref('posts').push({
+       text: value,
+       select: typePost,
+    });
+    posts(value, typePost, dataNew.key);
+    $('#post').val('');
+  });
 
 
-
-
-
-// // função click para postar
-//   $('#btn-post').click(function(){
-//     checkEmpty();
-//     neewsfeed();
-//     $('#post').val('');
-//     var publicacao = textarea();
-//     var type = select();
-//     database.ref('posts/' + userId).set({
-//         text: publicacao,
-//         select: type,
-//     });
-//   });
-//   // função para pegar o valor do textarea
-//   function textarea(){
-//     var value = $('#post').val();
-//     return value;
-//   };
-//   // função para pegar o select
-//   function select(){
-//     var typePost = $('.typePost').val();
-//     return typePost;
-//   };
-//   // função para habilitar o botão
-//   function checkEmpty(){
-//     var btnD = $('btn-post').attr('disabled');
-//     var btnV;
-//     var txtArea = textarea();
-//     if (txtArea === '' || txtArea.length > 140){
-//       btnD = true;
-//       btnV= $('btn-post').filter().css("background-color", '#969393');
-//     } else if(txtArea !=''){
-//       btnD = false;
-//       btnV= $('btn-post').filter().css( "background-color", "#1DA1F2");
-//     }
-//   };
-//   // função para criar a neewsfeed
-//   function neewsfeed(){
-//     var text = textarea();
-//     var type = select();
-//     // var postMain = $('.listPosts').append(`<article><p>${text}</p></article>`);
-//
-//     // var postMain = $('.listPosts').appendChild(.add('article').add('p').text(texT));
-//     // $(postMain).add('p').text(type);
-//
-//     var sectionPosts = document.querySelector('.listPosts');
-//     var publicar = document.createElement('article');
-//     var paragraph = document.createElement('p');
-//     paragraph.classList.add('publicar');
-//     paragraph.innerHTML = text;
-//     publicar.appendChild(paragraph);
-//     var paragraphTwo = document.createElement('p');
-//     paragraphTwo.innerHTML = type;
-//     sectionPosts.appendChild(publicar);
-//     document.getElementById('post').value = '';
-//   }
+  function posts(value, typePost, key){
+    $('.listPosts').prepend(`
+      <article>${value}<p>${typePost}</p></article>
+      <button data-edit-id="${key}">Editar</button>
+      <button data-delete-id="${key}">Excluir</button>
+      `);
+    $(`button[data-delete-id=${key}]`).click(function(){
+      $(this).parent().remove();
+      database.ref('posts/'+ key).remove();
+    })
+    $(`button[data-edit-id=${key}]`).click(function(){
+      var editText= prompt(`Altere o seu texto: ${value}`);
+      var palomita = $(`span[data-text-id=${key}]`).html(editText);
+      console.log(palomita);
+      database.ref('posts/' + key).set({
+        text: editText,
+        select: typePost
+      });
+    })
+  }
 });
